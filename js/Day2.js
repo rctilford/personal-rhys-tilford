1bf3bac40d6ad686e96f83d8d4603e39c37661d7
Rhys Tilford <rhystilford@gmail.com>
2023-11-06 19:50:32 -0500

    Completed new suggested features

--- js/Day2.js
+++ js/Day2.js
@@ -5,8 +5,19 @@ fileInputElement.addEventListener('change', handleFileSelect);
 function handleFileSelect(event) {
   const reader = new FileReader();
   reader.addEventListener("load", handleFileLoad);
-  reader.readAsText(event.target.files[0]).split('\r\n');
+  reader.readAsText(event.target.files[0]);
 }
+
+
+function handleFileLoad(event) {
+  // the "event" here is the loading of
+  // the file into the reader object
+  // created in hndleFileSelect.
+  // the target is that reader object,
+  // and the result is the file read
+  // in, as a string:
+
+const Input = event.target.result.split('\r\n');
 
 // Find the match points for each outcome.
 function getAllIndexes(arr, val) {
@@ -42,3 +53,4 @@ let sum = (cx*7)+(ay*8)+(bz*9)+(ax*4)+(by*5)+(cz*6)+(bx)+(cy*2)+(az*3);
 let resultsReport = `The total point value is: ${sum}.`;
 document.getElementById('result').textContent = resultsReport;
 
+};
